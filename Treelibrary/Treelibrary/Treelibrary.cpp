7293c953750e8f12371aaefd4bfa4cca29508ec5
Goryudyuma <umaretekyoumade@gmail.com>
2016-03-17 05:20:26 +0900

    今書いたところまで

--- Treelibrary/Treelibrary/Treelibrary.cpp
+++ Treelibrary/Treelibrary/Treelibrary.cpp
@@ -2,10 +2,131 @@
 
 #include "stdafx.h"
 
-using namespace System;
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 
-int main(array<System::String ^> ^args)
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+//#include <bits/stdc++.h>
+using namespace std;
+
+template <typename D, typename T = D>
+class Tree
 {
-    Console::WriteLine(L"Hello World");
-    return 0;
+public:
+	Tree( long long int size = 0, D InitialValue = D() ) {
+		long long int n = 1LL;
+		CalcTree.emplace_back( vector<T>( n ) );
+		while( n <= size ) {
+			n <<= 1LL;
+			CalcTree.emplace_back( vector<T>( n ) );
+		}
+		Data.resize( n, InitialValue );
+		MaxSize = n;
+		NowIndex = 0LL;
+	}
+
+
+
+	long long int t() {
+		return 10;
+	}
+
+private:
+	vector<vector<T>> CalcTree;
+	vector<D> Data;
+	long long int MaxSize;
+	long long int NowIndex;
+};
+
+template<typename D, typename T = D>
+class B :Tree<D, T>
+{
+};
+
+int main() {
+	Tree<int, pair<int, int>> x;
+	cout << x.t() << endl;
 }
